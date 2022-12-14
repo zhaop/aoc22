9b645484e522546b8ca0464ce558fdb00256caea
Patrick Zhao <zhaopatrick@gmail.com>
2022-12-14 12:05:28 +0100

    Add day 14 variant with HashMap

--- src/day14.rs
+++ src/day14.rs
@@ -1,9 +1,9 @@
-use std::{cmp, io::stdin};
+use std::{cmp, io::stdin, collections::HashMap};
 
 use itertools::{Itertools, MinMaxResult::MinMax};
 
-fn drop_sand_until<F>(grid: &mut Vec<Vec<char>>, source: (usize, usize), condition: F) -> usize
-where F: Fn(&Vec<Vec<char>>, (usize, usize)) -> bool {
+fn drop_sand_until<F>(grid: &mut HashMap<(i32, i32), char>, source: (i32, i32), condition: F) -> usize
+where F: Fn(&HashMap<(i32, i32), char>, (i32, i32)) -> bool {
     (0..).skip_while(|_| {
         let (mut i, mut j) = source;
         loop {
@@ -13,24 +13,40 @@ where F: Fn(&Vec<Vec<char>>, (usize, usize)) -> bool {
                 return false;
             }
 
-            if grid[i+1][j] == '.' {
+            if !grid.contains_key(&(i+1, j)) {
                 (i, j) = (i + 1, j);
-            } else if grid[i+1][j-1] == '.' {
+            } else if !grid.contains_key(&(i+1, j-1)) {
                 (i, j) = (i + 1, j - 1);
-            } else if grid[i+1][j+1] == '.' {
+            } else if !grid.contains_key(&(i+1, j+1)) {
                 (i, j) = (i + 1, j + 1);
             } else {
-                grid[i][j] = 'o';
+                grid.insert((i, j), 'o');
                 return true;
             }
         }
     }).next().unwrap()
 }
 
-fn print_grid(grid: &Vec<Vec<char>>) {
-    for row in grid {
-        for char in row {
-            print!("{}", char);
+fn get_bounds(grid: &HashMap<(i32, i32), char>) -> ((i32, i32), (i32, i32)) {
+    let ibounds = match grid.keys().map(|(i, _)| i).minmax() {
+        MinMax(&a, &b) => (a, b),
+        _ => todo!(),
+    };
+    let jbounds = match grid.keys().map(|(_, j)| j).minmax() {
+        MinMax(&a, &b) => (a, b),
+        _ => todo!(),
+    };
+    (ibounds, jbounds)
+}
+
+fn _print_grid(grid: &HashMap<(i32, i32), char>) {
+    let (ibounds, jbounds) = get_bounds(grid);
+    for i in (ibounds.0)..(ibounds.1+1) {
+        for j in (jbounds.0)..(jbounds.1+1) {
+            match grid.get(&(i, j)) {
+                Some(c) => print!("{}", c),
+                None => print!("."),
+            }
         }
         println!();
     }
@@ -47,32 +63,22 @@ pub fn main() {
             .collect_vec()
         )
         .collect_vec();
-    let xbounds = match rocks.iter().flatten().map(|(x, _)| x).minmax() {
-        // Include x = 500 in bounds + 150 margin on each side
-        MinMax(&a, &b) => (cmp::min(a, 500) - 150, cmp::max(b, 500) + 150 + 1),
-        _ => todo!(),
-    };
-    let ybounds = match rocks.iter().flatten().map(|(_, y)| y).minmax() {
-        // Include y = 0 in bounds + 1 margin on each side
-        MinMax(&a, &b) => (cmp::min(a, 0) - 1, cmp::max(b, 0) + 2),
-        _ => todo!(),
-    };
-    let mut grid = (0..(ybounds.1 - ybounds.0)).map(|_| vec!['.'; (xbounds.1 - xbounds.0) as usize]).collect_vec();
+    let mut grid: HashMap<(i32, i32), char> = HashMap::new();
     for rock in &rocks {
         for line in rock.windows(2) {
             if line[0].0 == line[1].0 {
                 // Vertical
                 let x = line[0].0;
                 for y in cmp::min(line[0].1, line[1].1)..(cmp::max(line[0].1, line[1].1) + 1) {
-                    let (i, j) = (y - ybounds.0, x - xbounds.0);
-                    grid[i as usize][j as usize] = '#';
+                    let (i, j) = (y, x);
+                    grid.insert((i, j), '#');
                 }
             } else if line[0].1 == line[1].1 {
                 // Horizontal
                 let y = line[0].1;
                 for x in cmp::min(line[0].0, line[1].0)..(cmp::max(line[0].0, line[1].0) + 1) {
-                    let (i, j) = (y - ybounds.0, x - xbounds.0);
-                    grid[i as usize][j as usize] = '#';
+                    let (i, j) = (y, x);
+                    grid.insert((i, j), '#');
                 }
             } else {
                 panic!("Diagonal line {:?}", line);
@@ -80,31 +86,31 @@ pub fn main() {
         }
     }
 
-    let source = ((0 - ybounds.0) as usize, (500 - xbounds.0) as usize);
+    let source = (0, 500);
     let h = grid.len();
     let grains_dropped_until_leak = drop_sand_until(
         &mut grid,
         source,
-        |_, (i, _)| i == h - 1,
+        |_, (i, _)| i == h as i32 - 1,
     );
 
-    print_grid(&grid);
+    // _print_grid(&grid);
     println!("{}", grains_dropped_until_leak);
 
     // Add floor, clear sand, resimulate
-    grid.push(vec!['#'; grid[0].len()]);
-    for row in grid.iter_mut() {
-        for char in row {
-            if *char == 'o' {
-                *char = '.';
-            }
-        }
+    let (ibounds, jbounds) = get_bounds(&grid);
+    for j in (jbounds.0 - 500)..(jbounds.1 + 500) {
+        let i = ibounds.1 + 2;
+        grid.insert((i, j), '#');
     }
+    let sand_locations = grid.iter().filter_map(|(&key, &value)| if value == 'o' { Some(key) } else { None }).collect_vec();
+    sand_locations.iter().for_each(|loc| { grid.remove(&loc); });
+
     let grains_dropped_until_block = drop_sand_until(
         &mut grid,
         source,
-        |grid, _| grid[source.0][source.1] != '.',
+        |grid, _| grid.contains_key(&source),
     );
-    print_grid(&grid);
+    // _print_grid(&grid);
     println!("{}", grains_dropped_until_block);
 }
